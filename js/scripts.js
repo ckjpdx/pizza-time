58098061b927ee530b57bba64bfc0a1ae6af6877
Chris Johnson <cknightjohnson@gmail.com>
2017-11-03 15:58:42 -0700

    add css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var addPizza = '<div class="new-pizza"><h3>select a size:</h3><select name="pizza-size"><option value="small">small $12</option><option value="medium">medium $15</option><option value="large">large $20</option></select><h3>add some toppings:</h3><p>( + $2 each )</p><input class="pizza-toppings" name="select-topping" type="checkbox" value="pepperoni"><label>add pepperoni</label><input class="pizza-toppings" name="select-topping" type="checkbox" value="olives"><label>add olives</label><input class="pizza-toppings" name="select-topping" type="checkbox" value="jalapenos"><label>add jalapeños</label></div>';
+var addPizza = '<div class="new-pizza"><h3>new pizza</h3><p>select size:</p><select name="pizza-size"><option value="small">small $12</option><option value="medium">medium $15</option><option value="large">large $20</option></select><h3>add some toppings:</h3><p>( + $2 each )</p><input class="pizza-toppings" name="select-topping" type="checkbox" value="pepperoni"><label>add pepperoni</label><input class="pizza-toppings" name="select-topping" type="checkbox" value="olives"><label>add olives</label><input class="pizza-toppings" name="select-topping" type="checkbox" value="jalapenos"><label>add jalapeños</label></div>';
 var pizzaSizeArr = ['small', 'medium', 'large'];
 var pizzaCostArr = [12, 15, 20];
 
@@ -14,19 +14,15 @@ Pizza.prototype.setCostBySize = function () {
     }
   }
 };
-
 Pizza.prototype.totalCost = function () {
   this.totalCost = this.sizeCost + this.deliveryFee + this.toppings.length * 2;
 };
 
-function orderTotal(order, isDelivery){
+function orderTotal(order){
   var runningTotal = 0;
   order.forEach(function(pizza){
     runningTotal += pizza.totalCost;
   });
-  if (isDelivery) {
-    runningTotal += order.length * 3;
-  }
   return runningTotal;
 }
 
@@ -40,6 +36,10 @@ $(function () {
   });
   $('form#order-form').submit(function(event){
     event.preventDefault();
+    var isDelivery = false;
+    if ($('input#delivery-address').val()) {
+      isDelivery = true;
+    }
     $('div#order-div').slideUp();
     var order = [];
     $('div.new-pizza').each(function(){ // start each pizza process
@@ -49,7 +49,7 @@ $(function () {
         toppings.push($(this).val());
       });
       var pizza = new Pizza(size, toppings);
-      if ($('input#delivery-address').val()) {
+      if (isDelivery) {
         pizza.deliveryFee = 3;
       }
       pizza.setCostBySize();
@@ -59,11 +59,14 @@ $(function () {
       }
       order.push(pizza);
     }); // end each pizza process
-    $('#confirm-order').append('<h2>pizza order:</h2><h4>' + order.length + ' pizza(s) ordered:</h4>');
+    $('#confirm-order').append('<h2>confirmation</h2><h4>' + order.length + ' pizza(s) ordered:</h4>');
     order.forEach(function(pizza){
       $('#confirm-order').append('<p>' + pizza.size + ' pizza<br>w/ ' + pizza.toppings.join(", ") + '<span class="delivery-fee-span"><br>delivery fee: $' + pizza.deliveryFee + '</span></p><p><strong>$ ' + pizza.totalCost + '</strong></p><hr>');
     });
     $('#confirm-order').append('<h3>order total: $' + orderTotal(order) + '</h3>');
     console.log(order);
+    if (isDelivery) {
+      $('span.delivery-fee-span').show();
+    }
   }); // end submit form
 });
