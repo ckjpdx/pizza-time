66309452e1c8daafccfecdeb1b3b91a5c5239132
Chris Johnson <cknightjohnson@gmail.com>
2017-11-03 09:54:16 -0700

    add frontend to display size of pizza selected from order form

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
 function Pizza(size, toppings){
   this.size = size;
   this.toppings = toppings;
+  switch (size) {
+    case "small": this.cost = 12;
+    break;
+    case "medium": this.cost = 15;
+    break;
+    case "large": this.cost = 20;
+    break;
+  default:
+    console.log('pizza constructor error');
+  }
 }
 
 // FRONTEND
 $(function () {
-  
+  $('#order-form').submit(function(event){
+    event.preventDefault();
+    var order = [];
+    var size = $('input.pizza-size:checked').val();
+    var pizza = new Pizza(size);
+    order.push(pizza);
+    console.log(order);
+    order.forEach(function(item){
+      console.log(item.size);
+      $('#confirm-order').append('<p>' + item.size + '</p>');
+    });
+  });
 });
